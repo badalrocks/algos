db471f081379644f97fdf50c44c814301967df52
badalrocks <badalrocks@DESKTOP-SL2JC48>
2019-05-05 13:02:18 -0400

    Fix bug in isConnected. And method to get conn level between 2 vertices.

--- src/com/badalchowdhary/datastructures/graph/Graph.java
+++ src/com/badalchowdhary/datastructures/graph/Graph.java
@@ -2,7 +2,9 @@ package com.badalchowdhary.datastructures.graph;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 public class Graph
@@ -20,38 +22,100 @@ public class Graph
     {
         vertices.add(src);
         vertices.add(dest);
-        
+
         edges.computeIfAbsent(src, key -> new HashSet<Edge>());
         edges.get(src).add(new Edge(dest)); // unweighted edge
-        
+
         if (!isDirected)
         {
             edges.computeIfAbsent(dest, key -> new HashSet<Edge>());
             edges.get(dest).add(new Edge(src));
         }
     }
-    
+
     public boolean isConnected(Vertex src, Vertex dest)
     {
-        if (!vertices.contains(src) || !vertices.contains(dest)) {
+        if (!vertices.contains(src) || !vertices.contains(dest))
+        {
             throw new IllegalArgumentException();
         }
-        
-        return edges.get(src).contains(new Edge(dest));
+
+        Set<Edge> neighbors = edges.get(src);
+        Queue<Vertex> q = new LinkedList<>();
+
+        for (Edge e : neighbors)
+        {
+            q.offer(e.dest);
+        }
+
+        while (!q.isEmpty())
+        {
+            Vertex v = q.poll();
+
+            if (v.equals(dest))
+            {
+                return true;
+            }
+
+            for (Edge e : edges.get(v))
+            {
+                q.offer(e.dest);
+            }
+        }
+
+        return false;
+
     }
-    
+
+    public int getConnLevel(Vertex src, Vertex dest)
+    {
+        if (!vertices.contains(src) || !vertices.contains(dest))
+        {
+            throw new IllegalArgumentException();
+        }
+
+        Queue<Vertex> q = new LinkedList<>();
+        q.offer(src);
+
+        int level = 0;
+
+        while (!q.isEmpty())
+        {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++)
+            {
+                Vertex v = q.poll();
+                if (v.equals(dest))
+                {
+                    return level;
+                }
+
+                for (Edge e : edges.get(v))
+                {
+                    q.offer(e.dest);
+                }
+
+            }
+
+            level++;
+        }
+
+        return -1;
+    }
+
     public Set<Vertex> getNeighbors(Vertex v)
     {
         Set<Vertex> neighbors = new HashSet<>();
-        
-        for (Edge e: edges.get(v)) {
+
+        for (Edge e : edges.get(v))
+        {
             neighbors.add(e.dest);
         }
-        
+
         return neighbors;
     }
-    
-    
+
     public Graph buildGraph(boolean isDirected)
     {
         Graph g = new Graph(false);
@@ -60,15 +124,14 @@ public class Graph
         Vertex c = new Vertex("C");
         Vertex d = new Vertex("D");
         Vertex e = new Vertex("E");
-        
+
         g.addEdge(a, b);
         g.addEdge(b, c);
         g.addEdge(c, d);
         g.addEdge(d, e);
         g.addEdge(e, a);
-        
+
         return g;
     }
-    
 
 }
--- src/com/badalchowdhary/datastructures/graph/GraphUtil.java
+++ src/com/badalchowdhary/datastructures/graph/GraphUtil.java
@@ -24,9 +24,29 @@ public class GraphUtil
         Graph g = GraphUtil.buildGraph();
 
         System.out.println("A neighbors: " + g.getNeighbors(new Vertex("A")));
+        
         System.out.println("A connected to B: " + g.isConnected(new Vertex("A"), new Vertex("B")));
         System.out.println("B connected to A: " + g.isConnected(new Vertex("B"), new Vertex("A")));
+        System.out.println("A connected to D: " + g.isConnected(new Vertex("A"), new Vertex("D")));
         System.out.println("A connected to E: " + g.isConnected(new Vertex("A"), new Vertex("E")));
+        
+        System.out.println("A -> B connLevel: " + g.getConnLevel(new Vertex("A"), new Vertex("B")));
+        System.out.println("B -> A connLevel: " + g.getConnLevel(new Vertex("B"), new Vertex("A")));
+        System.out.println("A -> E connLevel: " + g.getConnLevel(new Vertex("A"), new Vertex("E")));
+        System.out.println("E -> A connLevel: " + g.getConnLevel(new Vertex("E"), new Vertex("A")));
+        System.out.println("A -> C connLevel: " + g.getConnLevel(new Vertex("A"), new Vertex("C")));
+        
+        // console output
+//        A neighbors: [B]
+//        A connected to B: true
+//        B connected to A: true
+//        A connected to D: true
+//        A connected to E: true
+//        A -> B connLevel: 1
+//        B -> A connLevel: 1
+//        A -> E connLevel: 4
+//        E -> A connLevel: 4
+//        A -> C connLevel: 2               
     }
 
 }
